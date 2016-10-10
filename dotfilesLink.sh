60e3ca6746d3d3881043fecc0e722cd4892b0d42
koyakuwa <koyakuwa@gmail.com>
2016-10-10 17:00:26 +0900

    Create dotfilesLink.sh

---
+++ dotfilesLink.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/.bash_functions ~/.bash_functions
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
