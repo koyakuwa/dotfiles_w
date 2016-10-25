d538647b7162c67627b56e387afbf5179eb26501
koyakuwa <koyakuwa@gmail.com>
2016-10-25 14:27:00 +0900

    anything

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
-ln -sf ~/dotfiles/.bash_functions ~/.bash_functions
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+ln -sf ~/dotfiles_w/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles_w/.bash_functions ~/.bash_functions
+ln -sf ~/dotfiles_w/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles_w/.emacs.d ~/.emacs.d
