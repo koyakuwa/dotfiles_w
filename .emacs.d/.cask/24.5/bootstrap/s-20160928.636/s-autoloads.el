32fd69784c69f92872245cf62ad7845718398017
koyakuwa <koyakuwa@gmail.com>
2017-02-18 16:33:20 +0900

    update

--- .emacs.d/.cask/24.5/bootstrap/s-20160928.636/s-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/.cask/24.5/bootstrap/s-20160928.636/s-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/.cask/24.5/bootstrap/s-20160928.636/s.el")
-;;;;;;  (22537 56572 610000 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
--- .emacs.d/.cask/25.1/bootstrap/commander-20140120.1052/commander-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; commander-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/.cask/25.1/bootstrap/commander-20140120.1052/commander-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/.cask/25.1/bootstrap/commander-20140120.1052/commander.el")
-;;;;;;  (22588 20305 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; commander-autoloads.el ends here
