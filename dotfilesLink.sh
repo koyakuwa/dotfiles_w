8c479d19da57a04bc03c5991a69533fb46306609
koyakuwa <koyakuwa@gmail.com>
2017-04-22 21:17:01 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf ~/src/dotfiles_w/.zsh_aliases ~/.zsh_aliases
-ln -sf ~/src/dotfiles_w/.zsh_functions ~/.zsh_functions
-ln -sf ~/src/dotfiles_w/.zprofile ~/.zprofile
-ln -sf ~/src/dotfiles_w/.zshrc ~/.zshrc
-ln -sf ~/src/dotfiles_w/.tmux.conf ~/.tmux.conf
-ln -sf ~/src/dotfiles_w/.spacemacs ~/.spacemacs
+ln -sf ~/src/github.com/koyakuwa/dotfiles_w/.zsh_aliases ~/.zsh_aliases
+ln -sf ~/src/github.com/koyakuwa/dotfiles_w/.zsh_functions ~/.zsh_functions
+ln -sf ~/src/github.com/koyakuwa/dotfiles_w/.zprofile ~/.zprofile
+ln -sf ~/src/github.com/koyakuwa/dotfiles_w/.zshrc ~/.zshrc
+ln -sf ~/src/github.com/koyakuwa/dotfiles_w/.tmux.conf ~/.tmux.conf
+ln -sf ~/src/github.com/koyakuwa/dotfiles_w/.spacemacs ~/.spacemacs
